3db0beb0106f29b93867cb43e93b81570ac0ed5b
Meghna Jha <you@example.com>
2021-06-23 23:43:55 +0530

    deploy my react app

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+const Home = () => {
+  const {data,isPending,error}=useFetch('  http://localhost:8000/blogs')
+ 
+  return (
+    <div className="home">
+        {error && <div>{error}</div>}
+        {isPending && <div>loading....</div> }
+      {data && <BlogList blogs={data} />}
+    </div>
+  );
+}
+ 
+export default Home;
